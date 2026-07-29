4bc06ebb4edccdec23c28663c9bfd22af89e0dab
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2026-07-29 12:25:57 +0800

    commit

--- writing-secure-code/obj/Debug/net9.0/writing-secure-code.AssemblyInfo.cs
+++ writing-secure-code/obj/Debug/net9.0/writing-secure-code.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("writing-secure-code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5d72ac2e2ab395ce049577e858fb346148202b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340e36e4a01572e789351270278497acff706a25")]
 [assembly: System.Reflection.AssemblyProductAttribute("writing-secure-code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("writing-secure-code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
