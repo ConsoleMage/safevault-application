1b6c4fee3607d60b8ea4c08c47fe3278ba3128fd
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2026-08-03 07:37:42 +0800

    commit

---
+++ writing-secure-code/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace writing_secure_code.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
